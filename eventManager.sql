52ad5559a1bef1efb72ea214efcd4b2bfe77cec3
Corwin Van Deusen <35018264+swirty@users.noreply.github.com>
2024-11-21 09:20:31 -0500

    Parse Check
    Parses and inits DB properly

--- eventManager.sql
+++ eventManager.sql
@@ -1,10 +1,9 @@
-------- THIS WILL DROP THE TABLES!!!!!! -------
+------- THIS WILL DROP THE WHOLE DB TO CLEAR IT!!!!!! -------
 -- Creates and populates some data into the database.
-=======
+-------------------------------------------------------------
+DROP DATABASE IF EXISTS EventManager;
 CREATE DATABASE EventManager;
 USE EventManager;
-
-DROP TABLE *;
 
 -- User Table
 CREATE TABLE User (
@@ -16,44 +15,25 @@ CREATE TABLE User (
 
 -- Admin Table
 CREATE TABLE Admin (
-    userID INT NOT NULL FOREIGN KEY REFERENCES User(userID),
+    userID INT NOT NULL,
     adminID INT AUTO_INCREMENT PRIMARY KEY,
+	FOREIGN KEY (userID) REFERENCES User(userID)
 );
 
 -- Coordinator Table
 CREATE TABLE Coordinator (
-    userID INT NOT NULL FOREIGN KEY REFERENCES User(userID),
-    eventCoordinatorID INT AUTO_INCREMENT PRIMARY KEY
+    userID INT NOT NULL,
+    coordinatorID INT AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY (userID) REFERENCES User(userID)
 );
 
 -- Attendee Table
 CREATE TABLE Attendee (
-    userID INT NOT NULL FOREIGN KEY REFERENCES User(userID),
+    userID INT NOT NULL,
     userEmail VARCHAR(100) NOT NULL,
     phoneNumber VARCHAR(15) NOT NULL,
-    attendeeID INT AUTO_INCREMENT PRIMARY KEY
-);
-
--- Invite Table
-CREATE TABLE Invitation (
-    attendeeID INT NOT NULL,
-    eventID INT NOT NULL,
-    status ENUM('Pending', 'Accepted', 'Declined') DEFAULT 'Pending',
-    PRIMARY KEY (attendeeID, eventID),
-    FOREIGN KEY (attendeeID) REFERENCES Attendee(attendeeID),
-    FOREIGN KEY (eventID) REFERENCES Events(eventID)
-);
-
--- Feedback Table
-CREATE TABLE Feedback (
-    attendeeID INT NOT NULL,
-    eventID INT NOT NULL,
-    comments VARCHAR(300),
-    rating ENUM(1,2,3,4,5) DEFAULT 5,
-    feedbackDate DATETIME NOT NULL,
-    PRIMARY KEY (attendeeID, eventID),
-    FOREIGN KEY (attendeeID) REFERENCES Attendee(attendeeID),
-    FOREIGN KEY (eventID) REFERENCES Events(eventID)
+    attendeeID INT AUTO_INCREMENT PRIMARY KEY,
+    FOREIGN KEY (userID) REFERENCES User(userID)
 );
 
 -- Event Table
@@ -66,5 +46,27 @@ CREATE TABLE Event (
     location VARCHAR(30) NOT NULL,
     coordinatorID INT NOT NULL,
     FOREIGN KEY (coordinatorID) REFERENCES Coordinator(coordinatorID)
+);
+
+-- Invite Table
+CREATE TABLE Invitation (
+    attendeeID INT NOT NULL,
+    eventID INT NOT NULL,
+    status ENUM('Pending', 'Accepted', 'Declined') DEFAULT 'Pending',
+    PRIMARY KEY (attendeeID, eventID),
+    FOREIGN KEY (attendeeID) REFERENCES Attendee(attendeeID),
+    FOREIGN KEY (eventID) REFERENCES Event(eventID)
+);
+
+-- Feedback Table
+CREATE TABLE Feedback (
+    attendeeID INT NOT NULL,
+    eventID INT NOT NULL,
+    comments VARCHAR(300),
+    rating ENUM("1","2","3","4","5") DEFAULT "5",
+    feedbackDate DATETIME NOT NULL,
+    PRIMARY KEY (attendeeID, eventID),
+    FOREIGN KEY (attendeeID) REFERENCES Attendee(attendeeID),
+    FOREIGN KEY (eventID) REFERENCES Event(eventID)
 );
 
