4e97037a7ab6cfc34de954af1bf29e53622fda78
Corwin Van Deusen <corwinvandeusen1205@gmail.com>
2024-11-30 12:38:38 -0500

    fix extra comma

--- eventManager.sql
+++ eventManager.sql
@@ -10,7 +10,7 @@ CREATE TABLE User (
     userID INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('Admin', 'Coordinator', 'Attendee') NOT NULL,
+    role ENUM('Admin', 'Coordinator', 'Attendee') NOT NULL
 );
 
 -- Admin Table
